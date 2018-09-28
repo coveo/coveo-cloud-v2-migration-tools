ad054f663208ae907e1515b712397052decc9455
Francis Mireault <fmireault@coveo.com>
2018-09-28 13:26:44 -0700

    Use cloud client base class for v1

--- client/cloud_v1.py
+++ client/cloud_v1.py
@@ -1,8 +1,10 @@
 import json
 import requests
 
+from client.environment import CloudClient
 
-class CloudV1:
+
+class CloudV1(CloudClient):
     def __set_platform_url(self, env):
         if env == env.DEV:
             url = 'cloudplatformdev.coveo.com'
@@ -15,32 +17,16 @@ class CloudV1:
         self.platform_url = f'https://{url}'
         print(f'[CloudV1] Using environement {env}')
 
-    def __get_url(self, url):
-        return f'{self.platform_url}/{url}'
-
     def __init__(self, env, org_id, access_token):
+        super().__init__('CloudV1', env, org_id, access_token)
         self.__set_platform_url(env)
-        self.org_id = org_id
-        self.headers = {'Authorization': f'Bearer {access_token}'}
 
     def sources_get(self):
-        url = self.__get_url(f'rest/workgroups/{self.org_id}/sources')
-        print(f'>> Request to "{url}"')
-        response = requests.get(url, headers=self.headers)
-        print(f'>> Response (status: {response.status_code}): {response.text}')
-        if response.status_code != 200:
-            raise Exception(f'[CloudV1] Error retrieving fields for org id {self.org_id}')
-        return json.loads(response.text)
+        return self.do_get(f'rest/workgroups/{self.org_id}/sources')
 
     def fields_get(self) -> list:
-        url = self.__get_url(f'rest/workgroups/{self.org_id}/fields')
-        print(f'>> Request to "{url}"')
-        response = requests.get(url, headers=self.headers)
-        print(f'>> Response (status: {response.status_code}): {response.text}')
-        if response.status_code != 200:
-            raise Exception(f'[CloudV1] Error retrieving fields for org id {self.org_id}')
-        return json.loads(response.text)
+        return self.do_get(f'rest/workgroups/{self.org_id}/fields')
 
-    def fields_get_for_source(self, sourceId):
+    def fields_get_for_source(self, source_id):
         fields = self.fields_get()
-        return [field for field in fields if field["sourceId"] == sourceId]
+        return [field for field in fields if field["sourceId"] == source_id]
